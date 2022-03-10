3b7426004d2b9829da8fef5f687d0a134a87ee34
BullMooseDev <koleervine@gmail.com>
2022-03-10 13:36:49 -0700

    adding psuedo code

---
+++ index.js
@@ -0,0 +1,46 @@
+/* need inquirer 
+
+questions will be as follows: 
+-view all departments
+-view all roles
+-view all employees
+-add department
+-add role
+-add an employee
+-update employee info
+
+make user of oop like in team profile generator
+
+make a function to view table of departments names and id's for "view all departments"
+
+make a function to see a table for "view all roles" which will hold:
+-job title
+-role id
+-department that role belongs to
+-salary of role
+
+make a function to view all employees table which will hold:
+-employee id
+-first name
+-last name
+-job title
+-departent
+-salary
+-managers of employee
+
+make a function for adding a department name to the database
+
+make a funciton for adding a role which will include:
+-name
+-salary
+-department it belongs to
+
+make a function for adding an employee to the database which will include:
+-first name
+-last name
+-role
+-manager
+
+make a function for updating a employee which will grab it based on id:
+-update their role via multiple choice?
+ */
